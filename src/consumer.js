dc7a1da5979e8c4919f6a63d2217a77edf75dd26
Rizki <kikymaulana00@gmail.com>
2024-02-03 11:20:08 +0700

    change to prod  ip

--- src/consumer.js
+++ src/consumer.js
@@ -9,7 +9,7 @@ const init = async () => {
   const mailsender = new MailSender();
   const listener = new Listener(notesService, mailsender);
 
-  const connection = await amqp.connect('amqp://localhost');
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
 
   await channel.assertQueue('export:notes', {
